7a534fc9aa94c47e67ddb917f1231ae6c04d1b45
Jyotsna Gorle <jyotsnagorle@gmail.com>
2022-06-12 11:54:46 +0200

    session week4 work

--- main_controller.py
+++ main_controller.py
@@ -2,7 +2,15 @@ from models.student import Student
 from models import physics_teacher
 from views.school_students import SchoolStudents
 
+from models.subject import SchoolSubject
+
+# Student is data type / Class is a data type - eg. strings, Int, floats
 student = Student(name="Jyotsna", age=29, class_number=3)
+# access class variables
+print(student.age) # 29
+print(student.class_number) # 3
+print(student.name) # Jyotsna
+
 SchoolStudents().enroll_student(student)
 SchoolStudents().all_students()
 
@@ -12,3 +20,7 @@ physics_teacher_1 = physics_teacher.PhysicsTeacher(name="Mia", lab_number="101")
 
 print("Teacher details:")
 print("name:"+physics_teacher_1.name, "lab number:" + physics_teacher_1.get_lab_number())
+
+# create a new object of type SchoolSubject and print its name
+school_subject = SchoolSubject(name="Physics")
+print(school_subject.name)
